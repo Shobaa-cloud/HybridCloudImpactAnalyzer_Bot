ce142ca04de67d282cca755f6669a8740903e7df
sasiprabha Velusamy <sasiprabhavelusamy@gmail.com>
2026-08-08 08:46:42 +0530

    Initial commit

---
+++ compare.py
@@ -0,0 +1,29 @@
+import json
+
+# Read old configuration
+with open("old_config.json", "r") as file:
+    old_config = json.load(file)
+
+# Read new configuration
+with open("new_config.json", "r") as file:
+    new_config = json.load(file)
+
+print("========== Configuration Change Detection ==========\n")
+
+change_found = False
+
+for key in old_config:
+
+    if old_config[key] != new_config[key]:
+
+        print("Change Detected!")
+        print("---------------------------")
+        print("Setting :", key)
+        print("Old Value :", old_config[key])
+        print("New Value :", new_config[key])
+        print()
+
+        change_found = True
+
+if not change_found:
+    print("No configuration changes found.")
